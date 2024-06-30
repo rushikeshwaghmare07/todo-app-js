127c8e043375afd7aa637331de5761168eb21c0d
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-06-30 11:51:50 +0530

    update store.js

--- store.js
+++ store.js
@@ -3,19 +3,54 @@ const store = {
     {
       id: "1",
       title: "Complete Task A",
-      completed: false
+      completed: false,
     },
     {
       id: "2",
       title: "Read Books",
-      completed: true
+      completed: true,
     },
     {
       id: "3",
       title: "Writing Code",
-      completed: true
+      completed: true,
+    },
+  ],
+};
+
+const storeHandler = {
+  get(target, property) {
+    return target[property];
+  },
+  set(target, property, value) {
+    target[property] = value;
+    if (property == "todos") {
+      window.dispatchEvent(new Event("todoschange"));
     }
-  ]
+    localStorage.setItem("store", JSON.stringify(store));
+    return true;
+  },
+};
+
+const storeProxy = new Proxy(store, storeHandler);
+
+function addTodo(newTodo) {
+  storeProxy.todos = [...storeProxy.todos, newTodo];
 }
 
-export default store;
+function deleteTodo(id) {
+  storeProxy.todos = storeProxy.todos.filter((todo) => todo.id !== id);
+}
+
+function toggleCompleted(id, completed) {
+  storeProxy.todos = storeProxy.todos.map((todo) => {
+    if (todo.id === id) {
+      return { ...todo, completed: completed };
+    } else {
+      return todo;
+    }
+  });
+}
+
+export { addTodo, deleteTodo, toggleCompleted };
+export default storeProxy;
