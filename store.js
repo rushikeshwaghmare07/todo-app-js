986d948cb5a7b9a12c3a66115ac40bd83acf4013
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-06-30 10:41:39 +0530

    added todos object

---
+++ store.js
@@ -0,0 +1,21 @@
+const store = {
+  todos: [
+    {
+      id: "1",
+      title: "Complete Task A",
+      completed: false
+    },
+    {
+      id: "2",
+      title: "Read Books",
+      completed: true
+    },
+    {
+      id: "3",
+      title: "Writing Code",
+      completed: true
+    }
+  ]
+}
+
+export default store;
